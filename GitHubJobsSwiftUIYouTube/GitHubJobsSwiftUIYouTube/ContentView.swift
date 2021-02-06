2a43c123c8dddbc077c44b8433d692b1b6db69a7
Eugene Berezin <35341885+Eugeneberezin@users.noreply.github.com>
2021-02-05 16:26:51 -0800

    finished

--- GitHubJobsSwiftUIYouTube/GitHubJobsSwiftUIYouTube/ContentView.swift
+++ GitHubJobsSwiftUIYouTube/GitHubJobsSwiftUIYouTube/ContentView.swift
@@ -9,6 +9,7 @@ import SDWebImageSwiftUI
 import SwiftUI
 
 struct ContentView: View {
+    @EnvironmentObject var manager: PersistenceManager
     @State private var locationSearchTerm = ""
     @State private var positionSearchTerm = ""
     @State private var isEditing = false
@@ -47,14 +48,23 @@ struct ContentView: View {
                 ScrollView {
                     LazyVGrid(columns: [gridItem]) {
                         if viewModel.jobs.isEmpty {
-                            Text("Looking for a job? \nPlease enter job title and location to see what's available. ")
-                                .italic()
-                                .font(.title)
-                                .padding()
+                            VStack {
+                                Text("Looking for a job? \nPlease enter job title and location to see what's available. ")
+                                    .italic()
+                                    .font(.title)
+                                    .foregroundColor(.white)
+                            }
+                            .frame(width: 400, height: 200, alignment: .center)
+                            .background(Color(.systemBlue))
+                            .cornerRadius(12)
+                            .shadow(color: Color.black.opacity(0.6), radius: 6, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 3.0)
+                            .shadow(color: Color.black.opacity(0.5), radius: 4, x: 0, y: 0.2)
+
+                            
 
                         }
                         ForEach(viewModel.jobs) { job in
-                            JobView(job: job, image: WebImage(url: URL(string: job.companyLogo ?? "")), buttonColor: .gray)
+                            JobView(job: job, image: WebImage(url: URL(string: job.companyLogo ?? "")), buttonColor: manager.savedItems.contains(job) ? Color.yellow : Color.gray)
                             
                         }
                     }
--- GitHubJobsSwiftUIYouTube/SavedJobs.swift
+++ GitHubJobsSwiftUIYouTube/SavedJobs.swift
@@ -6,13 +6,41 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct SavedJobs: View {
     @EnvironmentObject var manager: PersistenceManager
+    @State var searchTerm = ""
+    let gridItem = GridItem(.flexible(minimum: 300, maximum: 400), spacing: 5, alignment: .leading)
     var body: some View {
-        List {
-            ForEach(manager.savedItems) { job in
-                Text(job.company)
+        NavigationView {
+            VStack {
+                SearchBarView(text: $searchTerm, placeHolder: "Job title")
+                ScrollView {
+                    LazyVGrid(columns: [gridItem]) {
+                        if manager.savedItems.isEmpty {
+                            VStack {
+                                Text("No saved jobs yet.")
+                                    .italic()
+                                    .font(.title)
+                                    .foregroundColor(.white)
+                            }
+                            .frame(width: 400, height: 200, alignment: .center)
+                            .background(Color(.systemBlue))
+                            .cornerRadius(12)
+                            .shadow(color: Color.black.opacity(0.6), radius: 6, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 3.0)
+                            .shadow(color: Color.black.opacity(0.5), radius: 4, x: 0, y: 0.2)
+
+                            
+
+                        }
+                        ForEach(manager.savedItems.filter {"\($0)".contains(searchTerm.lowercased()) || searchTerm.isEmpty}) { job in
+                            JobView(job: job, image: WebImage(url: URL(string: job.companyLogo ?? "")), buttonColor: manager.savedItems.contains(job) ? Color.yellow : Color.gray)
+                            
+                        }
+                    }
+                }
+                
             }
         }
     }
